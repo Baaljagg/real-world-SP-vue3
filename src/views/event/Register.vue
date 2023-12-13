084b3244f2eee3024e775fbd96036b24e0652a87
Baaljagg <desmaestro@gmail.com>
2023-12-13 15:50:14 +0100

    Programatic Navigation L6-end

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,20 @@
 <script setup>
+import { useRouter } from 'vue-router';
+const props = defineProps(["event"]);
+const router = useRouter()
 
-const props = defineProps(["event"])
+const register = () => {
+    //if registration API call is sucćessful
+    //push back to the event details
+    router.push({
+        name: 'event-details',
+    })
+
+}
 
 </script>
 
 <template>
     <p>Register for the event here</p>
+    <button @click="register">Register me!</button>
 </template>
